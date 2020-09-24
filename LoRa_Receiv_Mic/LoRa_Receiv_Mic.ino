3e70e7306a2b9feac1e2af4352c49568d6b5709a
Αλέξανδρος Πλατάνιος <alexandrosplatanios15@gmail.com>
2020-09-24 04:52:57 +0300

    Lora microphone stage 3

--- LoRa_Receiv_Mic/LoRa_Receiv_Mic.ino
+++ LoRa_Receiv_Mic/LoRa_Receiv_Mic.ino
@@ -2,21 +2,15 @@
 #include <LoRa.h>
 
 
-#include <Thread.h>
-#include <StaticThreadController.h>
-
-
-
-
 
 
 #define DI00 2
 #define PIN_SPI_RST   9
 #define PIN_SPI_SS    10
 /*
-#define PIN_SPI_MOSI  11
-#define PIN_SPI_MISO  12
-#define PIN_SPI_SCK   13
+  #define PIN_SPI_MOSI  11
+  #define PIN_SPI_MISO  12
+  #define PIN_SPI_SCK   13
 */
 
 
@@ -27,9 +21,27 @@ const int speacker_pin = 3;
 
 
 
+// Async Lora Dellay = 1937 micros
+// Sync Lora Delay = 376647 micros
+// Human voice frequency:
+// Male: 85 - 180 Hz
+// Femaly: 165 - 255 Hz
+
+int wait = 1261; // micros
+long previusTime = 0, currentTime = 0;
 
 
-void setup() { 
+const int len = 1000;
+byte data[len];
+
+
+
+
+
+
+
+
+void setup() {
   // Setup Serial
   Serial.begin(115200);
   while ( ! Serial );
@@ -37,8 +49,8 @@ void setup() {
   while ( ! LoRa.begin(433E6) );  // GREECE: 433,050–434,040 MHz  434,040–434,790 MHz
   LoRa.setSpreadingFactor(7); // 6 - 12
   LoRa.setSignalBandwidth(125E3); // 125, 250, 500
-  LoRa.setTxPower(16);  // Max 20
-  //LoRa.setSyncWord(3); // Big delay 
+  //LoRa.setTxPower(16);  // Max 20
+  //LoRa.setSyncWord(3); // Big delay
   LoRa.setCodingRate4(5); // 5,6,7,8
   //LoRa.setPreambleLength(3);
   //LoRa.crc();
@@ -52,54 +64,59 @@ void setup() {
 
 
 
-
-
-// 500 Hz
-int wait = 2000;
-long previusTime = 0, currentTime = 0;
 int counter = 0;
-const int len = 100;
-String data[len];
-
+boolean check_buffer = false;
 
 void loop() {
-
-  //long startTime = micros();
 
 
   // Get data from Lora
   int packetSize = LoRa.parsePacket();
   if ( packetSize ) {
+
+    // Receivin and data and push it in the buffer
     String message = "";
+    boolean check = false;
     while ( LoRa.available() ) {
-      char c = (char) LoRa.read();
-      if (c != '&') {
-        data[counter] += c;
-      } else {
-        counter++;
-      }
-      if ( counter == len ){
-        break;
+      message += (char) LoRa.read();
+      check = true;
+    }
+    if ( check ) {
+      data[counter] = (byte) message.toInt();
+      //Serial.println( "data[" + String(counter) + "]: " + String(data[counter]) );
+      counter++;
+      if ( counter == len-1 ) {
+        //Serial.println( "Buffer Ready" );
+        check_buffer = true;
+        counter = 0;
       }
     }
+
+    
+    if ( check_buffer ) {
+      Serial.println( "Saving data" );
+      check_buffer = false;
+      // Save Data To SD
+      for ( int i = 0; i < len; i++ ) {
+        Serial.println( data[i] );
+        analogWrite(speacker_pin, (int) data[i]);
+        // SD card apend this data to file
+                ////
+      }
+    }
+    
+      
   }
 
 
-
-  // Print data
-  for ( int i = 0; i < len; i++ ) {
-    Serial.println( data[i] );
-  }
   
+
+
+
   /*
-  // Play 500 Hz
-  currentTime = micros();
-  if ( (currentTime - previusTime) >= wait ) {
-    previusTime = currentTime;
-    Serial.println( message );
-    analogWrite( speacker_pin, message.toInt() );
-  }
+  Serial.println( data[counter] );
+  analogWrite(speacker_pin, data[counter]);
   */
+
   
-  //Serial.println( "Time: " + String(micros()-startTime) );
 }
--- LoRa_Send_Mic/LoRa_Send_Mic.ino
+++ LoRa_Send_Mic/LoRa_Send_Mic.ino
@@ -2,7 +2,6 @@
 #include <LoRa.h>
 
 
-
 #define DI00 2
 #define PIN_SPI_RST   9
 #define PIN_SPI_SS    10
@@ -12,70 +11,43 @@
 #define PIN_SPI_SCK   13
 */
 
- 
+
 
 
 const int speacker_pin = 3;
-
-const byte mic_pin = 0;  // A0
-const int MAX_RESULTS = 20;
-volatile int results[MAX_RESULTS];
-volatile int resultNumber;
-
-String buff = "";
-
-
-
-
-// ADC complete ISR Function
-ISR (ADC_vect) {
-  if (resultNumber >= MAX_RESULTS) {
-    ADCSRA = 0;  // Turn off ADC
-  } else {
-    results[resultNumber++] = ADC >> 2;
-  }
-}
-  
-EMPTY_INTERRUPT (TIMER1_COMPB_vect);
+const int mic_pin = A0;
 
 
 
 
 
-// Send DATA
-long previusTime = 0;
-int wait = 150;
-//int wait = 5000;
+// Async Lora Dellay = 1937 micros
+// Sync Lora Delay = 376647 micros
+// Human voice frequency:
+// Male: 85 - 180 Hz
+// Femaly: 165 - 255 Hz
 
-void send_data() {
-  
-  
-  // Play Local  76 kHz
-  for (int i = 0; i < MAX_RESULTS; i++) {
-    Serial.println( results[i] );
-    analogWrite( speacker_pin, results[i] );
-  }
-  
 
-  /*
-  // Send an array with len 20 and in the recever play the array with < 76 kHz >
-  if ( (millis() - previusTime) >= wait ) {
-    previusTime = millis();
-    for (int i = 0; i < MAX_RESULTS; i++) {
-      buff += String(results[i]) + "&";
-    }
-    Serial.println(buff);
-    Serial.println("\n\n");
-    LoRa.beginPacket();
-    LoRa.print( buff );
-    LoRa.endPacket(true);
-    buff = "";
-  }
-  */
-  
-}
+// 792 Hz ==> 792 samples per 1000 millis ==> 1000 samples per 1261 millis
+int sample_loop_time = 1261; // millis
+const int samples_len = 1000;
+byte data[samples_len]; // Buffer
 
-  
+
+
+
+/*
+ *   !!!!!
+ *          In Every Buffer Loop I lossing 38 Hz from LoRa
+ *   !!!!!
+ */
+
+
+
+
+
+// Update Lora Data
+void updateLoraData();
 
 
 
@@ -95,24 +67,37 @@ void setup() {
   //LoRa.setPreambleLength(3);
   //LoRa.crc();
   Serial.println("LoRa Sender Starts");
+}
 
-  
-  // Over Clocking , ADC Free Running mode
-  // Reset Timer 1
-  TCCR1A  = 0;
-  TCCR1B  = 0;
-  TCNT1   = 0;
-  TCCR1B  = bit (CS11) | bit (WGM12);  // CTC, prescaler of 8
-  TIMSK1  = bit (OCIE1B); 
-  OCR1A   = 39;    
-  OCR1B   = 39; // 20 uS - sampling frequency 50 kHz  
-  ADCSRA  =  bit (ADEN) | bit (ADIE) | bit (ADIF); // turn ADC on, want interrupt on completion
-  ADCSRA |= bit (ADPS2);  // Prescaler of 16  ==  ~76 kHz sampling rate
-  ADMUX   = bit (REFS0) | ( mic_pin & 7 );
-  ADCSRB  = bit (ADTS0) | bit (ADTS2);  // Timer/Counter1 Compare Match B
-  ADCSRA |= bit (ADATE);   // turn on automatic triggering
-  
 
+
+
+
+
+
+void loop() {
+
+    // In every 1261 millis get 1000 samples
+    long startTime = millis();
+    int pointer = 0;
+    while ( (millis()-startTime) <= sample_loop_time ) {
+      // Get Data
+      data[pointer] = (byte) analogRead(mic_pin) >> 2; // 110 micros blocking time
+      //analogWrite(speacker_pin, (int) data[pointer]);
+      //Serial.println( "data[" + String(pointer) + "]: " + data[pointer] );
+      pointer++;
+      if ( pointer ==  samples_len-1) {
+        Serial.println("Pointer stacks");
+        break;
+      }
+    }
+
+
+
+    // Update Lora Data
+    updateLoraData();
+
+    
     
 }
 
@@ -124,49 +109,16 @@ void setup() {
 
 
 
-
-void loop() {
-
-  
-  
-  while (resultNumber < MAX_RESULTS) {  }
-
-  
-  send_data();
-  /*
-  for (int i = 0; i < MAX_RESULTS; i++) {
-    //buff += String(results[i]) + "&";
-    Serial.println( results[i] << 2 );
-    analogWrite( speacker_pin, results[i] << 2 );
+// Update Lora Data
+void updateLoraData() {
+  analogWrite(speacker_pin, 0);
+  for ( int i = 0; i < samples_len; i++ ) {
+    Serial.println( "data[" + String(i) + "]: " + data[i] );
+    //analogWrite(speacker_pin, (int) data[i]);
+    LoRa.beginPacket();
+    LoRa.print( data[i] );
+    LoRa.endPacket(true);
+    long startTime = millis();
+    while ( (millis()-startTime) <= 33 );
   }
-  buff = "";
-  */
-  
- 
-  resultNumber = 0; // reset counter
-  ADCSRA =  bit (ADEN) | bit (ADIE) | bit (ADIF)| bit (ADPS2) | bit (ADATE); // turn ADC ON
-  
-
-
-
-
-  /*
-  // Count max frquency
-  long t0, t;
-  t0 = micros();
-  for(int i=0; i<1000; i++) {
-    analogRead(A0);
-  }
-  t = micros()-t0;  // calculate elapsed time
-
-  Serial.print("Time per sample: ");
-  Serial.print((float)t/1000);
-  Serial.println(" millis");
-  Serial.print("Frequency: ");
-  Serial.println((float)1000*1000000/t);
-  Serial.println();
-  delay(2000);
-  */
-  
-
 }
